6a79599000d44d69733661516211387aab644163
Florent.Bourgeois <florent.bourgeois@uha.fr>
2020-03-31 23:30:22 +0200

    first commit : add initial structure to the projet

---
+++ src/main/java/fr/cci/formationobjet/java/PaintFront/PaintFrontApplication.java
@@ -0,0 +1,13 @@
+package fr.cci.formationobjet.java.PaintFront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/cci/formationobjet/java/PaintFront/PaintFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cci.formationobjet.java.PaintFront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
